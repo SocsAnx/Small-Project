a04f0c01717b287cf4c84fdf9c2d7c3780891ade
ductrong5x5 <115746963+ductrong5x5@users.noreply.github.com>
2024-02-12 08:16:28 -0500

    Update Project1.py

--- Project1.py
+++ Project1.py
@@ -1,40 +1,13 @@
 print("Welcome to my computer quiz!")
 
-playing = input("Do you want to play? Yes or No: ")
-print("Answer: Yes or No")
-if playing.lower() != "yes":
+playing = input("Do you want to play? ")
+print(playing)
+
+if playing != "yes":
     quit()
 
-print("Okay! Let's play :)")
-score = 0
-print("Your score now is :  ",score, "!")
-answer = input("What does CPU stand for? ")
-if answer.lower() == "central processing unit":
-    print('Correct!')
-    score += 1
-else:
-    print("Incorrect!")
+print("Okay! Let's play ")
 
-answer = input("What does GPU stand for? ")
-if answer.lower() == "graphics processing unit":
-    print('Correct!')
-    score += 1
-else:
-    print("Incorrect!")
-
-answer = input("What does RAM stand for? ")
-if answer.lower() == "random access memory":
-    print('Correct!')
-    score += 1
-else:
-    print("Incorrect!")
-
-answer = input("What does PSU stand for? ")
-if answer.lower() == "power supply":
-    print('Correct!')
-    score += 1
-else:
-    print("Incorrect!")
-
-print("You got " + str(score) + " questions correct!")
-print("You got " + str((score / 4) * 100) + "%.")
+answer=input("what does CPU stnad for? ")
+if answer == "central processing unit":
+    print("Correct!")
